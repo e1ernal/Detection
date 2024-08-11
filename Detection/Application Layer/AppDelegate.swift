4b0732ea9c162e82b30d9b91afa65e118795c7b4
e1ernal <goodnightlemon@gmail.com>
2024-08-11 10:31:56 +0300

    added application layer

---
+++ Detection/Application Layer/AppDelegate.swift
@@ -0,0 +1,31 @@
+//
+//  AppDelegate.swift
+//  RoadSignDetection
+//
+//  Created by e1ernal on 18.06.2023.
+//
+
+import UIKit
+
+@main
+final class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+    
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        configure(window)
+        return true
+    }
+
+    private func configure(_ window: UIWindow) {
+        let viewController = MainVC()
+        let navigationController = UINavigationController()
+        navigationController.viewControllers = [viewController]
+        self.window = window
+        self.window?.rootViewController = navigationController
+        self.window?.makeKeyAndVisible()
+    }
+}
