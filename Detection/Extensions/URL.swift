72a9258961d5a6b0d1759920a1e4a5cf1045d857
e1ernal <goodnightlemon@gmail.com>
2024-11-29 13:20:15 +0300

    URL link to eng wiki

--- Detection/Extensions/URL.swift
+++ Detection/Extensions/URL.swift
@@ -8,5 +8,5 @@
 import Foundation
 
 extension URL {
-    static var roadSignsWiki = URL(string: "https://ru.wikipedia.org/wiki/%D0%94%D0%BE%D1%80%D0%BE%D0%B6%D0%BD%D1%8B%D0%B5_%D0%B7%D0%BD%D0%B0%D0%BA%D0%B8_%D0%A0%D0%BE%D1%81%D1%81%D0%B8%D0%B8")
+    static var roadSignsWiki = URL(string: "https://en.wikipedia.org/wiki/Road_signs_in_Russia")
 }
